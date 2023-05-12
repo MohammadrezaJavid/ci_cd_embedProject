5f6076d7201d33d0083281b09f5e60245fe38b00
mohammadrezajavid <javid.9@yahoo.com>
2023-05-12 13:35:27 +0330

    update users selectors

--- jlinux/jlinux/users/selectors.py
+++ jlinux/jlinux/users/selectors.py
@@ -1,4 +1,5 @@
 from .models import Profile, BaseUser
+from django.db.models import QuerySet
 
-def get_profile(user:BaseUser) -> Profile:
+def get_profile(user:BaseUser) -> QuerySet[Profile]:
     return Profile.objects.get(user=user)
