14a47b629d10b9bbd1d04184c7b0900384fb9bc2
mohammadrezajavid <javid.9@yahoo.com>
2023-05-12 17:47:57 +0330

    update api urls

--- jlinux/jlinux/api/urls.py
+++ jlinux/jlinux/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include(('jlinux.blog.urls', 'blog'))),
     path('users/', include(('jlinux.users.urls', 'users'))),
+    path('auth/', include(('jlinux.authentication.urls', 'auth'))),
 ]
