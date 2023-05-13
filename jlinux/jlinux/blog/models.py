4106c5b2f4f33cbb958105772b7ffaf847ccc764
mohammadrezajavid <javid.9@yahoo.com>
2023-05-13 16:21:25 +0330

    add post and subscription to models

--- jlinux/jlinux/blog/models.py
+++ jlinux/jlinux/blog/models.py
@@ -1,5 +1,49 @@
 from django.db import models
-from jlinux.common.models import BaseModel
+from django.core.exceptions import ValidationError
 
-class Product(BaseModel):
-    name = models.TextField(max_length=255)
+from jlinux.common.models import BaseModel
+from jlinux.users.models import BaseUser
+
+class Post(BaseModel):
+    
+    slug = models.SlugField(
+        primary_key= True,
+        max_length=  100,
+    )
+    
+    title = models.CharField(
+        max_length= 100,
+        unique=     True,
+    )
+    
+    content = models.CharField(
+        max_length= 1000,
+    )
+    
+    author = models.ForeignKey (
+        BaseUser,
+        on_delete= models.SET_NULL,
+        null= True,
+    )
+    
+    def __str__(self):
+        return self.slug
+    
+    
+class Subscription(models.Model):
+    subscriber= models.ForeignKey(BaseUser,
+                                  on_delete= models.CASCADE,
+                                  related_name= "subs")
+    target=     models.ForeignKey(BaseUser,
+                                  on_delete= models.CASCADE,
+                                  related_name= "targets")
+    
+    class Meta:
+        unique_together= ('subscriber', 'target')
+        
+    def clean(self):
+        if self.subscriber == self.target:
+            raise ValidationError({"subscriber": ("subscriber can't be equal to target")})
+        
+    def __str__(self):
+        return f"{self.subscriber.email}- {self.target.email}"
